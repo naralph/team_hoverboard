a3dbfdddffaf144fe382c31e2bc088c9b4be2b70
Crystiliser <jjbryant@fullsail.edu>
2017-06-27 15:10:47 -0400

    Added score

---
+++ VR Hoverboard/Assets/PlayerScoreScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScoreScript : MonoBehaviour
+{
+    //variable to get to the score manager
+    [SerializeField] GameManager manager;
+
+    //amount to increase the score by
+    public int scoreIncreaseAmount = 1;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Ring")
+        {
+            manager.scoreScript.increaseScore(scoreIncreaseAmount);
+        }
+    }
+    
+}
--- VR Hoverboard/Assets/Scripts/Managers/ScoreManager.cs
+++ VR Hoverboard/Assets/Scripts/Managers/ScoreManager.cs
@@ -6,15 +6,22 @@ public class ScoreManager : MonoBehaviour
 {
     // Using Serializable allows us to embed a class with sub properties in the inspector.
     [System.Serializable]
+    
     public class Multipliers
     {
         public float speedMultiplier;
         public float consecutiveRingMultiplier;
 
+        public float getCollectiveMultiplier()
+        {
+            return speedMultiplier * consecutiveRingMultiplier;
+        }
+
         public Multipliers(float sMul, float crMul) { speedMultiplier = sMul; consecutiveRingMultiplier = crMul; }
     }
 
     Multipliers ScoreMultipliers = new Multipliers(1.0f, 1.0f);
+    public int score = 0;
 
     //this will get called by our game manager
     public void SetupScoreManager()
@@ -26,5 +33,14 @@ public class ScoreManager : MonoBehaviour
     void Update()
     {
 
+    }
+
+    public void increaseScore(int value)
+    {
+        score += (value * (int)ScoreMultipliers.getCollectiveMultiplier());
+    }
+    public void decreaseScore(int value)
+    {
+        score -= value;
     }
 }
