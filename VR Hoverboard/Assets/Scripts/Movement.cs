826d1a9e48fc6cafe25535f0921147c29d922875
Nathan Ralph <nathan.andrew.ralph@hotmail.com>
2017-06-27 22:30:46 -0400

    Work on improving gamepad controls.

--- VR Hoverboard/Assets/Scripts/Movement.cs
+++ VR Hoverboard/Assets/Scripts/Movement.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    private float moveRate = 0.1f;
+    private float rotationRate = 10.0f;
 
-    public float moveRate = 10.0f;
+    private float minSpeed = 0.1f;
+    private float maxSpeed = 10.0f;
+    private float idleSpeed = 0.1f;
 
     private Transform position;
 
@@ -18,16 +22,43 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("w"))
-            position.Translate(position.forward * Time.deltaTime * moveRate);
+        //keyboard
+        //if (Input.GetKey("w"))
+        //    position.Translate(position.forward * Time.deltaTime * moveRate);
 
-        else if (Input.GetKey("s"))
-            position.Translate(-position.forward * Time.deltaTime * moveRate);
+        //else if (Input.GetKey("s"))
+        //    position.Translate(-position.forward * Time.deltaTime * moveRate);
 
-        else if (Input.GetKey("d"))
-            position.Translate(position.right * Time.deltaTime * moveRate);
+        //else if (Input.GetKey("d"))
+        //    position.Translate(position.right * Time.deltaTime * moveRate);
 
-        else if (Input.GetKey("a"))
-            position.Translate(-position.right * Time.deltaTime * moveRate);
+        //else if (Input.GetKey("a"))
+        //    position.Translate(-position.right * Time.deltaTime * moveRate);
+
+
+        //gamepad left joystick
+        //position.Translate(Input.GetAxis("LHorizontal") * position.right * Time.deltaTime * moveRate);
+
+
+
+        float lVertVal = Input.GetAxis("LVertical");
+        float currSpeed = moveRate * -Input.GetAxis("LVertical");
+
+        print(lVertVal + " JOYSTICK VAL");
+        print(currSpeed + " SPEED VAL");
+
+        //getAxis returns a floating value that is in between -1 and 1
+        //leaning down on the board, accelerating
+        //if (Input.GetAxis("LVertical") < 0.0f && currSpeed > maxSpeed)
+        //    currSpeed = maxSpeed;
+
+        ////leaning back on the board, decelerating
+        //if (Input.GetAxis("LVertical") > 0.0f && currSpeed < minSpeed)
+        //    currSpeed = minSpeed;
+        
+        position.Translate(position.forward * currSpeed);
+
+        //gamepad right joystick
+        position.rotation = Quaternion.AngleAxis(Input.GetAxis("RVertical") * rotationRate, position.right);
     }
 }
