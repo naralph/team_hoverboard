f4b0d1b67bcdc5b1d0118f71443af5e65ec1c47e
Crystiliser <jjbryant@fullsail.edu>
2017-07-13 09:56:03 -0400

    Added screen fade in to scene changes

---
+++ VR Hoverboard/Assets/ScreenFade.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScreenFade : MonoBehaviour
+{
+    public float fadeTime = 2.0f;
+
+    //The initial screen color.
+    public Color fadeColor = new Color(0.01f, 0.01f, 0.01f, 1.0f);
+
+    public Material fadeMaterial = null;
+    private bool isFading = false;
+    private YieldInstruction fadeInstruction = new WaitForEndOfFrame();
+
+    // Initialize.
+    void Awake()
+    {
+        // create the fade material
+        fadeMaterial = new Material(Shader.Find("Oculus/Unlit Transparent Color"));
+    }
+
+    // Starts the fade in
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnLevelFinishedLoading;
+        StartCoroutine(FadeIn());
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnLevelFinishedLoading;
+    }
+
+    // Starts a fade in when a new level is loaded
+    void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
+    {
+        StartCoroutine(FadeIn());
+    }
+
+    // Cleans up the fade material
+    void OnDestroy()
+    {
+        if (fadeMaterial != null)
+        {
+            Destroy(fadeMaterial);
+        }
+    }
+
+    // Fades alpha from 1.0 to 0.0
+    IEnumerator FadeIn()
+    {
+        float elapsedTime = 0.0f;
+        fadeMaterial.color = fadeColor;
+        Color color = fadeColor;
+        isFading = true;
+        while (elapsedTime < fadeTime)
+        {
+            yield return fadeInstruction;
+            elapsedTime += Time.deltaTime;
+            color.a = 1.0f - Mathf.Clamp01(elapsedTime / fadeTime);
+            fadeMaterial.color = color;
+        }
+        isFading = false;
+    }
+
+    // Renders the fade overlay when attached to a camera object
+    void OnPostRender()
+    {
+        if (isFading)
+        {
+            fadeMaterial.SetPass(0);
+            GL.PushMatrix();
+            GL.LoadOrtho();
+            GL.Color(fadeMaterial.color);
+            GL.Begin(GL.QUADS);
+            GL.Vertex3(0f, 0f, -12f);
+            GL.Vertex3(0f, 1f, -12f);
+            GL.Vertex3(1f, 1f, -12f);
+            GL.Vertex3(1f, 0f, -12f);
+            GL.End();
+            GL.PopMatrix();
+        }
+    }
+}
