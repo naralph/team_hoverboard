c1c9c2324eb9f9e0f0929920ea337337ab8f6753
Crystiliser <jjbryant@fullsail.edu>
2017-08-01 16:18:15 -0400

    Rings in canyon level partially added(needs more), and controller switch script added

---
+++ VR Hoverboard/Assets/ControllerSwitchScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerSwitchScript : SelectedObject
+{
+
+    //runs while object is selected
+    override public void selectedFuntion()
+    {
+
+    }
+
+    //runs when object is deselected
+    override public void deSelectedFunction()
+    {
+
+    }
+
+    //runs when timer succeeds
+    override public void selectSuccessFunction()
+    {
+
+    }
+}
